820ac28717a9953e3031fbdbb08f3ec207929a6f
Pratiksha Biradar <79656969+Bpratiksha@users.noreply.github.com>
2021-09-08 11:35:58 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,31 +1,21 @@
 #Selection sort in Python
 
-def selectionSort(array, size):
-   
-    for step in range(size):
-        min_idx = step
-
-        for i in range(step + 1, size):
-         
+def sort(data):
+    try:
+        for i in range(len(data)):
+            temp=i
             # to sort in descending order, change > to < in this line
             # select the minimum element in each loop
-            if array[i] < array[min_idx]:
-                min_idx = i
-         
-        # put min at the correct position
-        (array[step], array[min_idx]) = (array[min_idx], array[step])
+            for j in range(i+1,len(data)):
+                if data[j]<data[temp]:
+                    temp=j
+             
+            # put min at the correct position
+            data[temp],data[i]=data[i],data[temp]   
 
+    except:
+        print("enter valid input")
 
-data = [5,3,8,6,7,2]
-size = len(data)
-selectionSort(data, size)
-print('Sorted Array in Ascending Order:')
-print(data)
-
-# for i in range(len(data)):
-    #     temp=i
-    #     for j in range(i+1,len(data)):
-    #         if data[j]<data[temp]:
-    #             temp=j
-        
-    #     data[temp],data[i]=data[i],data[temp]
+data=[34,7,98,2,6,1]
+sort(data)
+print(f"selection sort:=> {data}")
